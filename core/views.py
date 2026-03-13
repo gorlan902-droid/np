36aca1642ee29bf8c812d37261eabd0e1499d186
dohewghs <dohewghs@gmail.com>
2026-03-13 10:51:20 +0200

    add test button

--- core/views.py
+++ core/views.py
@@ -4,5 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_view(request):
+    status = request.GET.get('status')
     
-    return render(request, 'core/index.html')
+    if status == 'changed': 
+            message = "Текст змінено!"
+    else:
+            message = "просто"
+            
+    # Передаємо цей текст у шаблон через "контекст" (словник)
+    return render(request, 'core/index.html', {'display_message': message})
